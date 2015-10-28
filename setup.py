1379645008f9ed4a096a01f116b3e4f98c583bd2
ElGoreLoco <esegoreloco@gmail.com>
2015-10-28 19:57:55 +0100

    Mejorada la sintaxis en ciertas partes

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
-#from distutils.core import setup
-__version__="0.1.0"
 
-setup (name="Pomodoreando",
-       version=__version__,
-       author="ElGoreLoco",
-       author_email="esegoreloco@gmail.com",
-       url="https://github.com/ElGoreLoco/Pomodoreando",
-       description="Aplicación para manejar tus pomodoros",
-       long_description="Pomodoreando es un programa pensado para manejar tus pomodoros de una manera rápida y sencilla. Además de su función de control de pomodoros, puedes utilizarlo como un cronómetro normal.",
-       license="MIT",
-       packages=["pomodoreando"],
-       scripts=["scripts/pomodoreando"],
-       install_requires="docopt")
+__version__ = "0.1.0"
+
+setup(name="Pomodoreando",
+      version=__version__,
+      author="ElGoreLoco",
+      author_email="esegoreloco@gmail.com",
+      url="https://github.com/ElGoreLoco/Pomodoreando",
+      description="Aplicación para manejar tus pomodoros",
+      long_description="Pomodoreando es un programa pensado para manejar tus \
+pomodoros de una manera rápida y sencilla. Además de su función de control de\
+ pomodoros, puedes utilizarlo como un cronómetro normal.",
+      license="MIT",
+      packages=["pomodoreando"],
+      scripts=["scripts/pomodoreando"],
+      install_requires="docopt")
