e51c272cd574344fa88ad13a455c9c09f5182694
ElGoreLoco <esegoreloco@gmail.com>
2015-11-29 15:07:57 +0100

    Nuevo sistema de cronómetro, más estable y exacto, y cambios varios.

--- pomodoreando/pomodoreando.py
+++ pomodoreando/pomodoreando.py
@@ -3,20 +3,20 @@
     pomodoreando [-ci] <intervalo>...
 
 Opciones:
-    -h --help                Muestra la ayuda
-    -v --version             Muestra la versión
-    -i --infinito            Hace que los intervalos sean infinitos
-    -c --continuo            Hace que no tengas que confirmar para que comience
-                             el siguiente intervalo
-    -p --pomodoros NUMERO    El número de pomodoros que correrá [default: 1]
-    -d --desc_largo          Hace que cada 4 pomodoros haya un descanso largo
+    -h --help               Muestra la ayuda
+    -v --version            Muestra la versión
+    -i --infinito           Hace que los intervalos sean infinitos
+    -c --continuo           Hace que no tengas que confirmar para que comience
+                            el siguiente intervalo
+    -p --pomodoros NUMERO   El número de pomodoros que correrá [default: infinitos]
+    -d --desc_largo         Hace que cada 4 pomodoros haya un descanso largo
 """
 
 import os
 import curses
 import threading
+import time
 from sys import argv
-from time import sleep
 from docopt import docopt
 
 from datos import *
@@ -54,7 +54,7 @@ def lista_intervalos():
                 lista.append(15)
             else:
                 lista.append(5)
-        return "pomodoro", lista
+        return { "intervalo": lista, "tipo": "pomodoro", "infinito": argumentos["--pomodoros"] == "infinitos" }
     else:
         """Pasa los intervalos a segundos (dependiendo de la última letra
         del argumento) y los añade a una lista que se retorna."""
@@ -73,42 +73,52 @@ def lista_intervalos():
                 except ValueError:
                     print("El intervalo no ha sido reconocido.\n",
                           "Vuelva a intentarlo.")
-        return "intervalo", lista
+        return { "intervalo": lista, "tipo": "intervalo", "infinito": argumentos["--infinito"] }
+
+
+argumentos = lista_intervalos()
 
 
 def numeroAletra(segundos):
+    """Transforma los segundos en una string utilizable"""
     m, s = divmod(segundos, 60)
     h, m = divmod(m, 60)
-    return "%02d:%02d:%02d" % (h, m, s)
+    letra = "%02d:%02d:%02d" % (h, m, s)
+    if segundos < 60:  # en segundos
+        return letra[6:]
+    elif segundos < 3600:  # con minutos
+        return letra[3:]
+    else:  # con horas
+        return letra
 
 
-class cronometro(threading.Thread):
-    def run(self):
-        self.ahora = ""
+class cronometro():
+    def __init__(self):
+        self.inicio = int(time.time())
         self.parado = False
-        self.segundos = 0
-        while True:
-            while not self.parado:
-                self.segundos += 1
-                self.letra = numeroAletra(self.segundos)
+        self.tiempo_parado = 0
 
-                # transforma los segundos en una string utilizable
-                if self.segundos < 60:  # en segundos
-                    self.ahora = self.letra[6:]
-                elif self.segundos < 3600:  # con minutos
-                    self.ahora = self.letra[3:]
-                else:  # con horas
-                    self.ahora = self.letra
-                sleep(1)
+    def segundos(self):
+        if self.parado:
+            return (self.inicio_parado - self.inicio) - self.tiempo_parado
+        else:
+            return (int(time.time()) - self.inicio) - self.tiempo_parado
+
+    def ahora(self):
+        return numeroAletra(self.segundos())
 
     def cambiar_estado(self):
         if self.parado:
             self.parado = False  # seguir xq está parado
+            self.final_parado = int(time.time())
+            self.tiempo_parado += self.final_parado - self.inicio_parado
         elif not self.parado:
             self.parado = True  # parar xq no está parado
+            self.inicio_parado = int(time.time())
 
     def reiniciar(self):
-        self.segundos = 0
+        self.inicio = time.time()
+
 
 cronometro = cronometro()
 
@@ -158,28 +168,26 @@ def pene():
     dibujar("p", "rosa")
 
 
-def main():
-    global cronometro
-    global x, y
-    cronometro.start()
-
+def contar_intervalo(pos):
     while True:
         entrada = pantalla.getch()
-        dibujar(cronometro.ahora)
 
-        if entrada == ord('t'):
+        if entrada == ord('t') or cronometro.segundos == argumentos["intervalo"][pos]:
             break
         elif entrada == ord('p'):
             cronometro.cambiar_estado()
 
-        elif entrada == ord('s'):
-            cronometro.segundos = 0
-        elif entrada == ord('m'):
-            cronometro.segundos = 58
-        elif entrada == ord('h'):
-            cronometro.segundos = 3598
+        dibujar(cronometro.ahora())
 
-        sleep(0.01)
+        time.sleep(0.01)
+
+
+def main():
+    #while True:
+    for i in range(len(argumentos["intervalo"])):
+        contar_intervalo(i)
+        time.sleep(1)
+        cronometro.reiniciar()
 
 
 main()
